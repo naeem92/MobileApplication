3df652af21d9d05c014366b0c8820f847d894e47
Naeem Muhammad Akram <85845054+naeem92@users.noreply.github.com>
2023-04-02 22:18:51 +0500

    Create a TextButton
    Step:1 Add Text Button
    Step:2 Text Button to assign the color.

--- Xylophone/README.md
+++ Xylophone/README.md
@@ -1,16 +1,3 @@
-# xylophone
+# xylophone_2022_fall
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+<img src="https://github.com/abdullahwale/flutter3.0_2022_yt_tutorials/blob/main/xylophone_2022_fall/output/xylophone.png">
--- Xylophone/lib/main.dart
+++ Xylophone/lib/main.dart
@@ -1,4 +1,56 @@
-void main(){
+import 'package:flutter/material.dart';
+//import 'package:audioplayers/audioplayers.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
+//import 'package:audioplayer/audioplayer.dart';
+void main() {
+  runApp(MyXylophoneApp());
+}
 
 
+//CTRL + ALt + L
+class MyXylophoneApp extends StatelessWidget {
+  void myFunction(int num, String name){
+    final player = AssetsAudioPlayer();
+    player.open(
+      Audio("assets/note$num.wav"),
+    );
+    print(name);
+  }
+
+  Expanded Create_Button(int number,String name,String buttonText,Color colur){
+    return Expanded(child: ElevatedButton(
+      onPressed: (){
+        myFunction(number,name);
+      },
+      child: Text(buttonText),
+      style: ElevatedButton.styleFrom(
+        primary: colur, // This is what you need!
+      ),
+    ),);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+
+          title: Text("Naeem's Xylophone App"),
+
+          centerTitle: true,
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Create_Button(1,"","Brown",Colors.brown),
+            Create_Button(2,"","Red",Colors.red),
+            Create_Button(3,"","",Colors.orange),
+            Create_Button(4,"","",Colors.black),
+            Create_Button(5,"","",Colors.purple),
+            Create_Button(6,"","",Colors.pink),
+            Create_Button(7,"","",Colors.red),
+          ],),
+      ),
+    );
+  }
 }
